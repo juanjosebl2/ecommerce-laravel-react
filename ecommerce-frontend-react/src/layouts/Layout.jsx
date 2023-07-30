6277285ca79f94a45cac36c9037151785bde6b6f
juanjosebl2 <juanjosebl2@gmail.com>
2023-07-30 12:40:08 +0200

    Installation laravel with docker and react with vite, Configurate first react for first start

---
+++ ecommerce-frontend-react/src/layouts/Layout.jsx
@@ -0,0 +1,7 @@
+export default function Layout() {
+  return (
+    <div>
+      <h1>Hello</h1>
+    </div>
+  )
+}
---
+++ ecommerce-frontend-react/src/router.jsx
@@ -0,0 +1,11 @@
+import { BrowserRouter, createBrowserRouter } from 'react-router-dom'
+import Layout from './layouts/Layout';
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Layout />
+    }
+])
+
+export default router
