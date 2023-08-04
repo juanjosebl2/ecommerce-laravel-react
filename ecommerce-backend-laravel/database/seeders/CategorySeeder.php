34ee301659bbdd9417665025804c1aa507430987
juanjosebl2 <juanjosebl2@gmail.com>
2023-08-04 12:57:01 +0200

    started with laravel, added migration, seeder, model and controller for categories

---
+++ ecommerce-backend-laravel/database/seeders/CategorySeeder.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Database\Seeders;
+
+use Carbon\Carbon;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('categories')->insert([
+            'name' => 'All',
+            'icon' => 'all',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ]);
+
+        DB::table('categories')->insert([
+            'name' => 'Coffes',
+            'icon' => 'coffe',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ]);
+        
+        DB::table('categories')->insert([
+            'name' => 'Burguers',
+            'icon' => 'Burguer',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ]);
+        
+        DB::table('categories')->insert([
+            'name' => 'Pizzas',
+            'icon' => 'pizza',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ]);
+        
+        DB::table('categories')->insert([
+            'name' => 'Donuts',
+            'icon' => 'donut',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ]);
+        
+        DB::table('categories')->insert([
+            'name' => 'Cakes',
+            'icon' => 'cake',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ]);
+        
+        DB::table('categories')->insert([
+            'name' => 'Cookies',
+            'icon' => 'cookie',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ]);
+    }
+}
