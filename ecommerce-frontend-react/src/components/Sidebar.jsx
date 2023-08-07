37e0090d5b556c3c89501511d519adf36bf83ce8
juanjosebl2 <juanjosebl2@gmail.com>
2023-08-07 12:03:54 +0200

    fixed some bugs

--- ecommerce-frontend-react/src/components/Sidebar.jsx
+++ ecommerce-frontend-react/src/components/Sidebar.jsx
@@ -6,6 +6,14 @@ export const Sidebar = () => {
 
     const { categories } = useStore();
 
+    // Change array for put in first place category All
+    const allCategoryIndex = categories.findIndex(category => category.name === 'All');
+
+    if (allCategoryIndex !== -1) {
+        const allCategory = categories.splice(allCategoryIndex, 1)[0];
+        categories.unshift(allCategory);
+    }
+
     return (
         <aside className='md:w-72'>
             <div className='p-4'>
